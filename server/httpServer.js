626b899584896d2f1d815856906147f7d06b18d1
Ryan Jones <rpj5582@rit.edu>
2017-05-01 14:10:27 -0400

    Fixed http server to use correct port on heroku

--- server/httpServer.js
+++ server/httpServer.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const fs = require('fs');
 
-const PORT = 3000;
+const PORT = process.env.PORT || process.env.NODE_PORT || 3000;
 
 const onRequest = (request, response) => {
   switch (request.url) {
